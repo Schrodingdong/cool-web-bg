311e94a9f5530c8ec6491865f07aead0e71d3c3f
Schrodingdong <91998001+Schrodingdong@users.noreply.github.com>
2022-06-18 12:09:37 +0100

    adding updated files

--- script.js
+++ script.js
@@ -60,9 +60,10 @@ class Particle{
 class ParticleManager{
     particleList = [];
     bgOppacity = 1;
-    constructor(particleList,bgOppacity,distanceThresh){
+    constructor(particleList,bgOppacity,distanceThresh,lineColor){
         this.particleList = particleList;
-        this.distanceThresh = distanceThresh
+        this.distanceThresh = distanceThresh;
+        this.lineColor = lineColor;
         if (bgOppacity >=1) 
             this.bgOppacity = bgOppacity;
     }
@@ -94,13 +95,12 @@ class ParticleManager{
                         var distance = p.getDistanceFrom(q);
                         if ( distance <= this.distanceThresh) {
                             
-                            console.log("here")
                             ctx.beginPath();
                             ctx.moveTo(p.x,p.y);
                             ctx.lineTo(q.x,q.y);
                             var alphaValue = (1 - distance/this.distanceThresh)/this.bgOppacity;
-                            console.log(alphaValue)
-                            ctx.strokeStyle = 'rgba(250, 88, 182,'+alphaValue+')'
+                            var tmp = this.lineColor.split(')')[0];
+                            ctx.strokeStyle = tmp+','+alphaValue+')';
                             ctx.stroke();
                         }
                     }
@@ -109,8 +109,7 @@ class ParticleManager{
         }
     }
 }
-  
-  
+
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -127,7 +126,7 @@ window.addEventListener('resize',()=>{ //Resizing
 //---------------------------------------- FORGROUND ----------------------------------------
 //initiate the particles 
 r = 0;
-const PARTICLE_NUMBER_FG = 50;
+const PARTICLE_NUMBER_FG = 10;
 const SPEED_FACTOR_FG = 6;
 var particleList_fg = [];
 for (var i  = 0 ; i < PARTICLE_NUMBER_FG ; i++){
@@ -140,10 +139,10 @@ for (var i  = 0 ; i < PARTICLE_NUMBER_FG ; i++){
     particleList_fg.push(p);
 }
 //initialisz the particle manager :
-// todo : distance thresh should be size responsive
-var distanceThresh = 200;
+var distanceThresh = 700;
 var lineOppacity = 1;
-var p_manager_fg = new ParticleManager(particleList_fg,lineOppacity,distanceThresh);
+lineColor = "rgba(250, 88, 182)"
+var p_manager_fg = new ParticleManager(particleList_fg,lineOppacity,distanceThresh,lineColor);
 
 
 //---------------------------------------- BACKGROUND ---------------------------------------
@@ -153,16 +152,17 @@ const PARTICLE_NUMBER_BG = 20;
 const SPEED_FACTOR_BG = 3;
 var particleList_bg = [];
 for (var i  = 0 ; i < PARTICLE_NUMBER_BG ; i++){
-  pos_x = Math.random() * innerWidth;
-  pos_y = Math.random() * innerHeight;
-  p = new Particle(pos_x,pos_y,r,SPEED_FACTOR_BG);
-  particleList_bg.push(p);
+    pos_x = Math.random() * innerWidth;
+    pos_y = Math.random() * innerHeight;
+    p = new Particle(pos_x,pos_y,r,SPEED_FACTOR_BG);
+    particleList_bg.push(p);
 }
 
 //initialisz the particle manager :
-distanceThresh = 1000;
-lineOppacity = 10;
-p_manager_bg = new ParticleManager(particleList_bg,lineOppacity,distanceThresh);
+distanceThresh = 700;
+lineColor = "rgba(250, 88, 182)"
+lineOppacity = 7;
+p_manager_bg = new ParticleManager(particleList_bg,lineOppacity,distanceThresh,lineColor);
 
 
 //---------------------------------------- ANIMATION ----------------------------------------
@@ -172,117 +172,14 @@ function animate(){
     ctx.clearRect(0,0,innerWidth,innerHeight)
 
 
-
     particleList_fg.forEach(element => {
         element.moveParticle();
     });
     p_manager_fg.drawParticleLines(true);
-
-
 
     particleList_bg.forEach(element => {
         element.moveParticle();
     });
     p_manager_bg.drawParticleLines(true);
 }
-
-
-
-
-animate()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//------------------------------ GARBAGE ------------------------------
-
-
-
-
-// //initialise particles: 
-// NUMBER_PARTICLES = 10
-// particleList = []
-// for (let i  = 0; i < NUMBER_PARTICLES ; i++){
-//   //coordinates :
-//   const x = Math.random() * canvas.width;
-//   const y = Math.random() * canvas.height;
-//   let p = new Particle(x,y,canvas);
-//   p.drawParticle(); 
-//   particleList.push(p)
-// }
-
-
-// //draw lines between them :
-// NORMALISATION_THRESH = 200;
-
-// pointsToLink = []
-// min = 0
-// for (let i = 0; i < NUMBER_PARTICLES ; i ++){
-//   //i is the main point we will compare to the other ones
-//   p = particleList[i]
-
-//   //get list of all particles sorted from nearest to farthest :
-//   distanceList = []
-//   for (let j = 0; j < NUMBER_PARTICLES ; j++){
-//     if(i == j) continue;
-
-//   }
-
-
-
-
-
-//   //get nearest 5 :
-//   NEAREST_NUM = 5
-//   nearestList = []
-//   for (let j = 0; j < NUMBER_PARTICLES ; j++){
-//     if (nearestList.length >= 5) break
-//     if(i == j) continue;
-//     q = particleList[j]
-//     distance = p.getDistanceFrom(q)
-//     //link to nearest 4 points : 
-//     if (min  > distance)
-//       pointsToLink.push()
-
-
-
-//     //       console.log("distance : " , distance)
-//     //       thiccOfLine = 1-Math.min(NORMALISATION_THRESH,distance) / NORMALISATION_THRESH
-//     //       console.log("thiccOfLine : ",thiccOfLine)
-
-//     //       //drawLine :
-//     //       ctx.beginPath
-//     //       colorAndOppacity = 'rgba(0,0,0,'+thiccOfLine+')'
-//     //       console.log(colorAndOppacity) 
-//     //       ctx.strokeStyle = colorAndOppacity;
-//     //       ctx.moveTo(p.x,p.y);
-//     //       ctx.lineTo(q.x,q.y);
-//     //       ctx.stroke();
-//   }
-// }
-
-
-  
+animate()  
